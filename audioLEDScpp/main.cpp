3a05209d7beb637dfdc862b36712ba464f6e5a04
mindstorms1 <tacticalemu2@gmail.com>
2017-04-07 10:58:09 -0400

    initial commit from VS17

---
+++ audioLEDScpp/main.cpp
@@ -0,0 +1,13 @@
+// main.cpp
+//
+// Contributor(s): Kyle Krupinski
+//
+// Designed to take audio from windows recording device,
+// analyze, and process in order to create a data stream 
+// used to drive strips of WS2812B RGB LEDs via an ESP8266
+// based development board
+//
+//
+//
+//
+
